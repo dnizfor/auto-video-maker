5975faea768278570694a975c24fa5ef57749b52
dniz <dnizfor@gmail.com>
2023-03-03 17:10:03 +0300

    create fuction that conconate all clips

---
+++ utils/mergeAllClips.js
@@ -0,0 +1,11 @@
+import Ffmpeg from "fluent-ffmpeg";
+// concat 3 mp4s together using 2 500ms directionalWipe transitions
+
+const mergeAllClips = (videoNames, outputPath) => {
+  let mergedVideo = Ffmpeg();
+  videoNames.forEach((video) => (mergedVideo = mergedVideo.input(video)));
+
+  mergedVideo.on("end", () => console.log("ok")).mergeToFile(outputPath);
+};
+
+export default mergeAllClips;
