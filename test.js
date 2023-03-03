9cc7622d0baa01986a37cab5eb6c7759fa30dad9
dniz <dnizfor@gmail.com>
2023-03-03 17:14:13 +0300

    add gitignore file

--- test.js
+++
@@ -1,7 +0,0 @@
-import mergeAudios from "./utils/mergeAudios.js";
-
-const list = [
-  ...[...Array(3).keys()].map((a, index) => `./voices/${index}.mp3`),
-];
-console.log(list);
-mergeAudios(list);
