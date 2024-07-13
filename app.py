535cf31dbf3b598edc5a420e50562c2d3f4759b3
dnizfor <dnizfor@gmail.com>
2024-07-13 20:56:35 +0300

    ok

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ import json
 import os
 
 # Load JSON data from a file
-with open('lib/test.json', 'r', encoding="utf-8") as f:
+with open('lib/home.json', 'r', encoding="utf-8") as f:
     words_and_means = json.load(f)
 
 levels = ["A1", "A2", "B1", "B2", "C1"]
-levels = ["A1"]
+levels = [0]
+print(words_and_means[0])
 
 for level in levels:
+    index = 0
+
 
     words_of_level = [
         dic for dic in words_and_means if dic["level"] == level]
@@ -18,7 +21,8 @@ for level in levels:
     print(words_of_level)
 
     for item in words_of_level:
-        if (f'{item["word"]}.mp4' not in os.listdir("clips")):
-            create_video_clip(item['word'], item['mean'], "en", "tr")
+        if (f'{index}.mp4' not in os.listdir("clips")):
+            create_video_clip(item['word'], item['mean'], "en", "tr",index)
+        index +=1
 
-concatenate_clips_in_folder('clips', level, "assets/intro.mp4")
+    concatenate_clips_in_folder('clips', level, "assets/intro.mp4")
--- create_video_clips.py
+++ create_video_clips.py
@@ -3,22 +3,22 @@ from gtts import gTTS
 import os
 
 
-def create_video_clip(word, mean, lang1, lang2):
+def create_video_clip(word, mean, lang1, lang2,output_name):
     # Create audio files for word and mean
     word_audio = gTTS(text=word, lang=lang1)
-    word_audio.save(f"{word}.mp3")
+    word_audio.save(f"word.mp3")
     mean_audio = gTTS(text=mean, lang=lang2)
-    mean_audio.save(f"{word}-mean.mp3")
+    mean_audio.save(f"word-mean.mp3")
 
     # Create video clips for word and mean
     word_clip = TextClip(word, fontsize=150, color='black',
                          bg_color="white", size=(1920, 1080), font='assets/NotoSansSC-Medium.otf')
     word_clip = word_clip.set_duration(
-        3).set_audio(AudioFileClip(f"{word}.mp3"))
+        3).set_audio(AudioFileClip("word.mp3"))
     mean_clip = TextClip(f"{word}\n{mean}", fontsize=150,
                          color='black', bg_color="white", size=(1920, 1080), font='assets/NotoSansSC-Medium.otf')
     mean_clip = mean_clip.set_duration(
-        3).set_audio(AudioFileClip(f"{word}-mean.mp3"))
+        3).set_audio(AudioFileClip("word-mean.mp3"))
 
     # Concatenate the two clips
     final_clip = concatenate_videoclips([word_clip, mean_clip])
@@ -34,8 +34,8 @@ def create_video_clip(word, mean, lang1, lang2):
     final_clip = CompositeVideoClip([final_clip, logo_clip])
 
     # Write the result to a file
-    final_clip.write_videofile(f"clips/{word}.mp4", fps=1)
+    final_clip.write_videofile(f"clips/{output_name}.mp4", fps=1)
 
     # Delete the temporary audio files
-    os.remove(f"{word}.mp3")
-    os.remove(f"{word}-mean.mp3")
+    os.remove(f"word.mp3")
+    os.remove(f"word-mean.mp3")
